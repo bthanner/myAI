a7ad18bbc3dc5bc9ca724a7881f623e014cb69b7
bthanner <blthanner@gmail.com>
2025-02-24 20:12:00 -0500

    Update identity.ts
    trying name again

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
+export const OWNER_NAME: string = `Brenna`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `May D'Well`;
 export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_ROLE: string = `Personal Shopper`;
